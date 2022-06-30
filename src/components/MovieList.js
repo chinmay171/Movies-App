d19778eca60e11e3c57fdceedf201e208a4eef33
Chinmay <chinmaygupta171@gmail.com>
2022-07-01 01:16:17 +0530

    handle previous functionality

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,16 +1,43 @@
 import { Component } from "react";
-import { movies } from "../movieData";
+import axios from "axios";
 
 class MovieList extends Component{
     constructor(){
         super();
         this.state={
             hover:"",
-            pArr : [1]
+            pArr : [1],
+            movies: [],
+            currPage: 1
         };
     }
+    async componentDidMount(){
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=81242a2aa2066e052c78ec9ac1700c59&language=en-US&page=${this.state.currPage}`);
+        this.setState({
+            movies : [...res.data.results]
+        })
+    }
+
+    changeMovies= async()=>{
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=81242a2aa2066e052c78ec9ac1700c59&language=en-US&page=${this.state.currPage}`);
+        this.setState({
+            movies : [...res.data.results]
+        })
+    }
+    handleNext=()=>{
+        this.setState({
+            pArr: [...this.state.pArr,this.state.pArr.length+1],
+            currPage:this.state.currPage+1
+        },this.changeMovies);
+    }
+
+    handlePrevious=()=>{
+        this.setState({
+            currPage:this.state.currPage-1
+        },this.changeMovies);
+    }
+    
     render(){
-        let movieArr = movies.results;
         return(
             <>
             <div>
@@ -18,7 +45,7 @@ class MovieList extends Component{
             </div>
 
             <div className="movies-list">
-                {movieArr.map((movieEle)=>(
+                {this.state.movies.map((movieEle)=>(
                     <div className="card movie-card" onMouseEnter={()=>this.setState({hover : movieEle.id})} onMouseLeave= {()=>this.setState({hover : ""})}>
                         <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{height:'40vh',width:'20vw'}} className="card-img-top movie-img" alt="..."/>
                         <h5 className="card-title  movie-title">{movieEle.title}</h5>
@@ -33,11 +60,11 @@ class MovieList extends Component{
             <div style = {{display : "flex", justifyContent : "center"}}>
                 <nav aria-label="Page navigation example">
                 <ul className="pagination">
-                    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                    <li className="page-item"><a className="page-link" onClick={this.handlePrevious}>Previous</a></li>
                     {this.state.pArr.map((ele) =>(
-                        <li className="page-item"><a className="page-link" href="#">{ele}</a></li>
+                        <li className="page-item"><a className="page-link">{ele}</a></li>
                     ))}
-                    <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                    <li className="page-item"><a className="page-link" onClick={this.handleNext} >Next</a></li>
                 </ul>
                 </nav>
             </div>
